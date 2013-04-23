371a1c0dd2a842c7c414844535580993f95906a7
Rosario Di Marco <r.dimarco@solvingteam.it>
2013-04-23 16:53:48 +0200

    db_corsi

--- DB_CORSI.sql
+++ DB_CORSI.sql
@@ -1,29 +1,29 @@
-CREATE SCHEMA `DB_CORSI` ;
+CREATE SCHEMA `db_corsi` ;
 
-CREATE  TABLE `DB_CORSI`.`studenti` (
+CREATE  TABLE `db_corsi`.`studenti` (
   `matricola` VARCHAR(8) NOT NULL ,
   `nome` VARCHAR(45) NULL ,
   `cognome` VARCHAR(45) NULL ,
   PRIMARY KEY (`matricola`) );
 
-  CREATE  TABLE `DB_CORSI`.`corsi` (
+  CREATE  TABLE `db_corsi`.`corsi` (
   `id` INT NOT NULL ,
   `materia` VARCHAR(45) NULL ,
   PRIMARY KEY (`id`) );
 
-  CREATE  TABLE `DB_CORSI`.`prenotazioni` (
+  CREATE  TABLE `db_corsi`.`prenotazioni` (
   `cod_studente` VARCHAR(8) NULL ,
   `cod_corso` INT NULL ,
   INDEX `fk_prenotazione_studente_idx` (`cod_studente` ASC) ,
   INDEX `fk_prenotazione_corso_idx` (`cod_corso` ASC) ,
   CONSTRAINT `fk_prenotazione_studente`
     FOREIGN KEY (`cod_studente` )
-    REFERENCES `DB_CORSI`.`studenti` (`matricola` )
+    REFERENCES `db_corsi`.`studenti` (`matricola` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_prenotazione_corso`
     FOREIGN KEY (`cod_corso` )
-    REFERENCES `DB_CORSI`.`corsi` (`id` )
+    REFERENCES `db_corsi`.`corsi` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 	
