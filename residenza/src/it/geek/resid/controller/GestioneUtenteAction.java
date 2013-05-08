c44f49caee1731329c02e26ead8d4da2af172b16
Rosario Di Marco <r.dimarco@solvingteam.it>
2013-05-08 11:02:29 +0200

    ...

--- residenza/src/it/geek/resid/controller/GestioneUtenteAction.java
+++ residenza/src/it/geek/resid/controller/GestioneUtenteAction.java
@@ -1,9 +1,24 @@
 package it.geek.resid.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
 public class GestioneUtenteAction extends DispatchAction {
+	
+	Logger log = Logger.getLogger(GestioneUtenteAction.class);
 
-
+	public ActionForward inserimento(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+					throws Exception{
+		log.debug("inserimento...");
+		
+		return mapping.findForward("success");
+	}
 
 }
---
+++ residenza/src/it/geek/resid/form/UtenteForm.java
@@ -0,0 +1,19 @@
+package it.geek.resid.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class UtenteForm extends ActionForm {
+
+	private String method;
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	
+	
+	
+}
